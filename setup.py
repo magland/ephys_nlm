af3458552ca7bb5ef996f21df3fe58a872d726df
Jeremy Magland <jmagland@flatironinstitute.org>
2020-02-03 12:58:36 -0500

    deploy 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name = "ephys_nlm"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.1",
+    version="0.1.2",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Non-local means denoising of multi-channel electrophysiology timeseries.",
